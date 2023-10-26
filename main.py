30b9a3d2af103fed4fe11e6f01ff52b01ba636c6
AINDUSTRIES <contact.aindustries@gmail.com>
2023-10-26 16:34:00 +0200

    test of light module

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
-"""
-To get started, check out the "Device Simulator Express: Getting Started" command in the command pallete, which you can access with `CMD + SHIFT + P` For Mac and `CTRL + SHIFT + P` for Windows and Linux.
-
-Getting started with CPX and CircuitPython intro on:
-https://learn.adafruit.com/circuitpython-made-easy-on-circuit-playground-express/circuit-playground-express-library
-
-Find example code for CPX on:
-https://github.com/adafruit/Adafruit_CircuitPython_CircuitPlayground/tree/master/examples
-"""
-
-# import CPX library
+"""This code is meant to be copied over to code.py on the controller"""
 from adafruit_circuitplayground import cp
-import time
+import board
+import adafruit_tcs34725
+import math
 
-# Main loop
-while True:
-    cp.pixels[3] = (255,50,20)
-    time.sleep(0.5)
-    cp.pixels[3] = (40,255,80)
-    time.sleep(0.5)
-    pass
+class Bot():
+    def __init__(self):
+        i2c = board.I2C()
+        self.sensor = adafruit_tcs34725.TCS34725(i2c)
+        self.sensor.integration_time = 2.4
+        self.sensor.gain = 60
+
+    def is_white(self, colors):
+        if all([color > 70 for color in colors]):
+            return True
+        return False
+
+    def get_color(self):
+        color_rgb = self.sensor.color_raw
+        colors = [255/1024 * color for color in color_rgb]
+        return colors
+    
+    def change_led(self, i_list: list, color: list):
+        rgb = tuple(color[0:-1])
+        for i in i_list:
+            cp.pixels[i] = rgb
+
+if __name__ == "__main__":
+    bot = Bot()
+    for _ in range(0,200):
+        rgbs = bot.get_color()
+        print(rgbs)
+        print(bot.is_white(rgbs))
+        bot.change_led([0], rgbs)
